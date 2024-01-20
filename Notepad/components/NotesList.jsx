a2a1b0c0b49f30c4fa56ba500b88d5191639a1b1
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2024-01-20 21:11:28 +0530

    Update NotesList.jsx

--- Notepad/components/NotesList.jsx
+++ Notepad/components/NotesList.jsx
@@ -1,19 +1,19 @@
 import Note from './Note';
 import AddNote from './AddNote';
 import { v4 as uuid } from 'uuid';
-
+import { url } from './constants';
 import Search from './Search';
 import { useState, useEffect } from 'react';
 
 const NotesList = () => {
-  const [inputText, setInputText] = useState("");
+  const [input, setInput] = useState({ head: '', text: '' });
   const [searchText, setSearchText] = useState("");
   const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("Notes")) || []);
   const [editToggle, setEditToggle] = useState(null);
-
-  const editNote = (id, text) => {
+  const [addNote, setAddNote] = useState(false);
+  const editNote = (id, head, text) => {
     setEditToggle(id);
-    setInputText(text);
+    setInput({ head, text });
   }
 
   useEffect(() => {
@@ -25,49 +25,71 @@ const NotesList = () => {
     setNotes(newNotes);
   };
 
+  const isEmpty = () => {
+    if (input.head === '' && input.text === '') {
+      return true;
+    }
+    return false;
+  }
+
   const saveNote = () => {
+    setAddNote(false);
     const date = new Date();
 
     if (editToggle) {
       setNotes(notes.map((note) => (
         note.id === editToggle ?
-          { ...note, text: inputText, date: date.toLocaleDateString() }
+          { ...note, head: input.head, text: input.text, date: date.toLocaleDateString() }
           : note
       )))
     } else {
+      if (isEmpty()) return;
       setNotes((prevNotes) => [
         ...prevNotes, {
           id: uuid(),
-          text: inputText,
+          head: input.head,
+          text: input.text,
           date: date.toLocaleDateString(),
         }
       ])
     }
 
-    setInputText("")
+    setInput({ head: '', text: '' });
     setEditToggle(null);
   }
-  const filteredNotes = notes.filter((note) =>
-    note.text.toLowerCase().includes(searchText)
+
+  var filteredNotes = notes.filter((note) =>
+    note.head?.toLowerCase().includes(searchText)
   );
+
   return (
     <>
       <Search handleSearchNote={setSearchText} />
       <div className='notes-list'>
-        {filteredNotes.map((note) => (
-          editToggle === note.id ?
-            <AddNote key={note.id} inputText={inputText} setInputText={setInputText} saveHandler={saveNote} />
-            :
-            <Note
-              key={note.id}
-              id={note.id}
-              text={note.text}
-              date={note.date}
-              handleDeleteNote={deleteNote}
-              handleEditNote={editNote}
-            />
-        ))}
-        <AddNote inputText={editToggle === null ? inputText : ''} setInputText={editToggle === null ? setInputText : null} saveHandler={saveNote} />
+        {!addNote ? <img
+          className='icons'
+          src={url + 'plus.png'}
+          alt='Plus'
+          onClick={() => setAddNote(true)}
+        />
+          : <AddNote input={input} setInput={setInput} saveNote={saveNote} />}
+        {filteredNotes
+          .slice(0)
+          .reverse()
+          .map((note) => (
+            editToggle === note.id ?
+              <AddNote key={note.id} input={input} setInput={setInput} saveNote={saveNote} />
+              :
+              <Note
+                key={note.id}
+                id={note.id}
+                head={note.head}
+                text={note.text}
+                date={note.date}
+                handleDeleteNote={deleteNote}
+                handleEditNote={editNote}
+              />
+          ))}
       </div>
     </>
   );
