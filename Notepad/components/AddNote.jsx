01e878fba7865fa1bbfbbecc38598aab4d7c535d
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2024-01-20 21:10:58 +0530

    Update AddNote.jsx

--- Notepad/components/AddNote.jsx
+++ Notepad/components/AddNote.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import { useRef } from "react";
+import useOutsideClick from "../hooks/useOutsideClick";
 
-const AddNote = ({ inputText, setInputText, saveHandler }) => {
-	const char = 200;
-	const characterLimit = char - inputText.length;
+const AddNote = ({ input, setInput, saveNote }) => {
+	const wrapperRef = useRef(null);
+	const handleInputChange = (field, value) => {
+		setInput((prevInput) => ({ ...prevInput, [field]: value }));
+	};
+
+	useOutsideClick(wrapperRef, saveNote);
+
 	return (
-		<div className='note new'>
+		<div className='note new' ref={wrapperRef}>
+			<input placeholder='Title'
+				value={input.head}
+				onChange={e => handleInputChange('head', e.target.value)} />
 			<textarea
 				rows='8'
 				cols='10'
 				placeholder='Type to add a note...'
-				value={inputText}
-				onChange={e => setInputText(e.target.value)}
+				value={input.text}
+				onChange={e => handleInputChange('text', e.target.value)}
 			></textarea>
 			<div className='note-footer'>
-				<small>
-					{characterLimit} Remaining
-				</small>
-				<button className='save' onClick={saveHandler}>
+				<small>{input.text?.length} characters</small>
+				<button className='save' onClick={saveNote}>
 					Save
 				</button>
 			</div>
