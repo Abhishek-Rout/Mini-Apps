7e0e5bc0e6d6f0a9589428d207fc1045cc0ec867
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2024-03-07 22:44:07 +0530

    Update UrlShortner.jsx

--- src/UrlShortner/UrlShortner.jsx
+++ src/UrlShortner/UrlShortner.jsx
@@ -3,12 +3,21 @@ import styles from './UrlShortner.module.scss';
 import hash from '../utils/hash';
 
 function UrlShortner() {
+    const queryString = window.location.search.slice(1);
+    if(queryString !== '') {
+        window.location.replace('https://github.com/Abhishek-Rout/Mini-Apps/tree/main/src/utils');
+    }
     const [url, setUrl] = useState();
     const [shortendUrl, setShortenedUrl] = useState('');
     // Convert url into a hex
     const urlToHex = () => {
-        setShortenedUrl(hash(url));
+        // const header = 'https://abhishek-rout.github.io/Mini-Apps/short.io/?';
+        const header = 'http://127.0.0.1:5173/Short.io/?';
+        const hashedURL = hash(url);
+        const shortURL = header + hashedURL;
+        setShortenedUrl(shortURL);
     }
+
 
     return (
         <div className={styles.UrlShortner}>
