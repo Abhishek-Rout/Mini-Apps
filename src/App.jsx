271392eedefbaed0effde8c45237a6ce24114ae1
Abhishek Rout <64718836+Abhishek-Rout@users.noreply.github.com>
2024-03-25 22:03:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Notepad from './Notepad/Notepad';
 import Otpform from './Otp-input/Otpform';
 import UrlShortner from './UrlShortner/UrlShortner';
 import MotionSimulator from './MotionSimulator/MotionSimulator';
+import WeatherApp from './WeatherApp/WeatherApp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/otpform' element={<Otpform />} />
         <Route path='/short.io' element={<UrlShortner />} />
         <Route path='/simulator' element={<MotionSimulator />} />
+        <Route path='/weather' element={<WeatherApp />} />
       </Routes>
     </Router>
   );
